5150e9516a2a776484e5baf51fe2f1b7319157f3
Nir Yosef <nir1125111@gmail.com>
2018-10-24 21:02:11 +0300

    class Nucleus_function init

--- lesson 2/2/2/2/Nucleus.cpp
+++ lesson 2/2/2/2/Nucleus.cpp
@@ -1,7 +1,7 @@
 #include "Nucleus.h"
 #include <iostream>
 
-
+// class Gene
 // initialize
 void Gene::init(const unsigned int start, const unsigned int end, const bool on_complementary_dna_strand) {
 	this->_start = start;
@@ -33,4 +33,45 @@ void Gene::setEnd(const unsigned int end) {
 
 void Gene::setOnComplementaryDnaStrand(const bool onComplementaryDnaStrand) {
 	this->_on_complementary_dna_strand = onComplementaryDnaStrand;
+}
+
+// class Nucleus
+// init
+void Nucleus::init(const std::string dna_sequence) {
+	int i = 0, reliable = 1;
+	// checking if the input string is valid
+	if (dna_sequence.empty()) {
+		reliable = 0;
+	}
+	for (i = 0; i < dna_sequence.length(); i++) {
+		if (dna_sequence[i] != 'A' && dna_sequence[i] != 'G' && dna_sequence[i] != 'C' && dna_sequence[i] != 'T') { // can only contain letters AGCT
+			reliable = 0;
+		}
+	}
+	if (reliable) {
+		// initialize
+		this->_DNA_strand = dna_sequence;
+		// changing the string to the required complementary DNA strand
+		this->_complementary_DNA_strand = "";
+		for (i = 0; i < dna_sequence.length(); i++) {
+			switch (dna_sequence[i]) {
+			case 'A':
+				this->_complementary_DNA_strand.push_back('T');
+				break;
+			case 'T':
+				this->_complementary_DNA_strand.push_back('A');
+				break;
+			case 'C':
+				this->_complementary_DNA_strand.push_back('G');
+				break;
+			case 'G':
+				this->_complementary_DNA_strand.push_back('C');
+			// No need for default we already know what the options are
+			}
+		}
+	}
+	else { // string is not valid
+		std::cerr << "the string can only contain the letters: A, G, C, T" << std::endl;
+		_exit(1);
+	}
 }

a2aa3eb96f3e9bd6d5e4af23941f6e366029835e
Nir Yosef <nir1125111@gmail.com>
2018-12-28 15:25:59 +0200

    added getHeight in BSNode

--- lesson 9/9/9/main.cpp
+++ lesson 9/9/9/main.cpp
@@ -13,28 +13,16 @@ int main()
 	BSNode* bs = new BSNode("6");
 	bs->insert("2");
 	bs->insert("8");
-	bs->insert("3"); 
-	bs->insert("4");
+	bs->insert("3");
 	bs->insert("5");
 	bs->insert("9");
 	bs->insert("6");
-	std::cout << bs->getHeight();
-	/*curr = new BSNode(*bs);
-	std::cout << "bs: " << bs->getData() << "right:\n";
-	while (curr) {
-		std::cout << curr->getData() << " ";
-		curr = curr->getRight();
-	}
-	delete curr;
-	while (curr) {
-		std::cout << curr->getData() << " ";
-		curr = curr->getRight();
-	}*/
+	bs->insert("222");
 	
 
 	/*cout << "Tree height: " << bs->getHeight() << endl;
 	cout << "depth of node with 5 depth: " << bs->getLeft()->getRight()->getRight()->getDepth(*bs) << endl;
-	cout << "depth of node with 3 depth: " << bs->getLeft()->getRight()->getDepth(*bs) << endl;
+	cout << "depth of node with 3 depth: " << bs->getLeft()->getRight()->getDepth(*bs) << endl;*/
 
 
 	string textTree = "BSTData.txt";
@@ -42,8 +30,8 @@ int main()
 
 	system("BinaryTree.exe");
 	system("pause");
-	remove(textTree.c_str());*/
+	remove(textTree.c_str());
 	delete bs;
-
+	system("PAUSE");
 	return 0;
 }

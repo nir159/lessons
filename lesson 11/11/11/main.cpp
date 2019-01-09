2742096da8cb39a51a283ecdb9396752cb8d56c7
Nir Yosef <nir1125111@gmail.com>
2019-01-09 16:05:13 +0200

    --amendfinished 11

--- lesson 11/11/11/main.cpp
+++ lesson 11/11/11/main.cpp
@@ -16,8 +16,8 @@ int main()
 	vector<int> primes3 = callGetPrimes(93, 289);
 
 	callWritePrimesMultipleThreads(0, 1000, "primes2.txt", 2);
-	callWritePrimesMultipleThreads(0, 100000, "primes2.txt", 2);
-	callWritePrimesMultipleThreads(0, 1000000, "primes2.txt", 2);
+	//callWritePrimesMultipleThreads(0, 100000, "primes2.txt", 2);
+	//callWritePrimesMultipleThreads(0, 1000000, "primes2.txt", 2);
 
 	system("pause");
 	return 0;

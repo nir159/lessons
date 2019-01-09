773e382f39c9af430ebe92c89a9f47116d692ce6
Nir Yosef <nir1125111@gmail.com>
2019-01-09 14:43:42 +0200

    part C

--- lesson 11/11/11/main.cpp
+++ lesson 11/11/11/main.cpp
@@ -7,8 +7,12 @@ int main()
 	call_I_Love_Threads();
 
 	vector<int> primes1;
-	primes1 = callGetPrimes(58, 100);
+	primes1 = callGetPrimes(0, 10000);
 	printVector(primes1);
+	/*primes1 = callGetPrimes(0, 100000);
+	printVector(primes1);
+	primes1 = callGetPrimes(0, 1000000);
+	printVector(primes1);*/
 	vector<int> primes3 = callGetPrimes(93, 289);
 
 	//callWritePrimesMultipleThreads(1, 100000, "primes2.txt", 2);

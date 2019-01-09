d0c234f4a4884e5e80103e7f27b6b9725e5c04f7
Nir Yosef <nir1125111@gmail.com>
2019-01-09 16:03:52 +0200

    finished

--- lesson 11/11/11/main.cpp
+++ lesson 11/11/11/main.cpp
@@ -7,15 +7,17 @@ int main()
 	call_I_Love_Threads();
 
 	vector<int> primes1;
-	primes1 = callGetPrimes(0, 10000);
+	/*primes1 = callGetPrimes(0, 10000);
 	printVector(primes1);
-	/*primes1 = callGetPrimes(0, 100000);
+	primes1 = callGetPrimes(0, 100000);
 	printVector(primes1);
 	primes1 = callGetPrimes(0, 1000000);
 	printVector(primes1);*/
 	vector<int> primes3 = callGetPrimes(93, 289);
 
-	//callWritePrimesMultipleThreads(1, 100000, "primes2.txt", 2);
+	callWritePrimesMultipleThreads(0, 1000, "primes2.txt", 2);
+	callWritePrimesMultipleThreads(0, 100000, "primes2.txt", 2);
+	callWritePrimesMultipleThreads(0, 1000000, "primes2.txt", 2);
 
 	system("pause");
 	return 0;
--- lesson 11/11/11/threads.h
+++ lesson 11/11/11/threads.h
@@ -5,7 +5,10 @@
 #include<vector>
 #include <Windows.h>
 #include <thread>
+#include <ctime>
 
+#define ZERO 0
+#define ONE 1 
 #define START 2 
 
 using namespace std;
@@ -36,6 +39,9 @@ void getPrimes(int begin, int end, vector<int>& primes) {
 	bool prime = true;
 	while (begin <= end) { // loop over the numbers
 		prime = true;
+		if (begin == ZERO || begin == ONE) {
+			prime = false;
+		}
 		i = START;
 		while (i <= begin / START && prime) // check if the current number is not prime if not prime leaves the loop
 		{
@@ -59,6 +65,43 @@ vector<int> callGetPrimes(int begin, int end) {
 	return primes;
 }
 
+void writePrimesToFile(int begin, int end, ofstream& file) {
+	vector<int> primes;
+	std::vector<int>::iterator it;
+	if (!file.is_open()) { // check if the file has been opened
+		cout << "Error occurred" << endl;
+		exit(1);
+	}
+	getPrimes(begin, end, primes);
+	for (it = primes.begin(); it != primes.end(); ++it) {
+		file << *it << endl; // write to the file
+	}
+}
 
-void writePrimesToFile(int begin, int end, ofstream& file);
-void callWritePrimesMultipleThreads(int begin, int end, string filePath, int N);
+void callWritePrimesMultipleThreads(int begin, int end, string filePath, int N) {
+	std::clock_t start; 
+	double duration;
+	ofstream file;
+	std::thread thread;
+
+	file.open(filePath);
+	if (!file.is_open()) { // check if the file has been opened
+		cout << "Error occurred" << endl;
+		exit(1);
+	}
+
+	start = std::clock(); // calculate the time
+	// Write the primes to file
+	while (begin + N < end) {
+		thread = std::thread(writePrimesToFile, begin, begin+N, std::ref(file));
+		thread.detach();
+		begin += N+ONE; // because I don't want the same number to appear twice
+	}
+
+	thread = std::thread(writePrimesToFile, begin, end, std::ref(file));
+	thread.join(); // waiting for the last thread to finish
+
+	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC; // change to seconds
+
+	std::cout << "The total time it took to write to file is: " << duration << " seconds." << std::endl;
+}
